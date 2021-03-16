e1a59828cbe8b53e69cd1083c891a13370611704
Teddy Aleksov <teddy57603@gmail.com>
2021-03-16 18:43:20 +0200

    Add project files.

---
+++ ReflectionAndAttributes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hacker hacker = new Hacker();
+            Spy spy = new Spy();
+            Console.WriteLine(spy.StealFieldInfo("Stealer.Hacker", "username", "password"));
+        }
+    }
+}
---
+++ ReflectionAndAttributes/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classToInvestigate, params string[] fields)
+        {
+            Type type = Type.GetType(classToInvestigate);
+            object instance = Activator.CreateInstance(type, new object[]{ });
+
+            FieldInfo[] info = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static |
+                                              BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Class under investigation: {type.GetType().Name}");
+
+            foreach (var field in info.Where(x => fields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
